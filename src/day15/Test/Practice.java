c1976c7cbf488062115a9d4e8e395ff26951ed0b
857910915 <857910915@qq.com>
2018-08-07 09:14:23 +0800

    Day15 Practice

--- src/day15/Test/Practice.java
+++ src/day15/Test/Practice.java
@@ -1,10 +1,31 @@
 package day15.Test;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Practice {
 
 	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-
+		// 双色球
+		//初始化一个随机数对象
+		ThreadLocalRandom random=ThreadLocalRandom.current();
+		//初始化一个数组
+		int []num=new int[6];
+		//生成六个随机数
+		//判断是否有相同的数
+		for (int i = 0; i < num.length; i++) {
+			num[i]=random.nextInt(0,32);
+			for (int j = 0; j < i; j++) {
+				if (num[i]==num[j]) {
+					i--;
+					break;
+				}
+			}
+		}	
+		for(int tem:num){
+			System.out.print(tem+"\t");
+		}
 	}
+
+
 
 }
