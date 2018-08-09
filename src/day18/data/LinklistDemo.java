e772c1405d2a6166856afe2f7142408b391ca93c
857910915 <857910915@qq.com>
2018-08-09 15:58:04 +0800

    链表

---
+++ src/day18/data/LinklistDemo.java
@@ -0,0 +1,158 @@
+package day18.data;
+
+import javax.print.attribute.standard.PagesPerMinute;
+
+//节点类
+class Node{
+	//指向下个节点的引用
+	Node next;
+	//数据部分
+	String name;
+	int age;
+	//构造方法
+	public Node() {
+		// TODO 自动生成的构造函数存根
+	}
+	public  Node(String name,int age) {
+		this.name=name;
+		this.age=age;
+	}
+	@Override
+	public String toString() {
+		// TODO 自动生成的方法存根
+		System.out.println("姓名："+name+"\t\t年龄："+age);
+		return super.toString();
+	}
+}
+public class LinklistDemo {
+	//操作链表  增删查改
+	//声明一个头部节点
+	Node head=null;
+	//添加节点
+	public void addNode(Node node) {
+		//1.判断头部节点是否为空
+		if (head==null) {
+			//将头部指向node
+			head=node;
+			return;
+		}
+		//2.判断判断节点的下个节点是否为空
+		Node tmp=head;//把head赋值给一个临时变量
+		Node pre=null;//记录上个节点
+		while (tmp!=null) {
+			pre=tmp;
+			//遍历下个节点
+			tmp=tmp.next;
+		}		
+		//如果为空，把最新的节点赋值给当前node的下个节点
+		pre.next=node;
+	}
+	//删除节点
+	public void deleteNode(String name) {
+		//将头部节点赋值给tmp
+		Node tmp=head;
+		//声明上个节点
+		Node pre=null;
+		//判断节点是否为空
+		while (tmp!=null) {
+			//当前节点是否是要删除的节点
+			if (tmp.name.equals(name)) {
+				//删除节点
+				//判断要删除的是否为第一个节点
+				if (tmp==head) {
+					head=head.next;
+					break;
+				}
+				//不是头部节点
+				pre.next=tmp.next;
+			}
+			//不是要删除的节点
+			pre=tmp;
+			tmp=tmp.next;
+		}
+	}
+	//查询  通过内容查询索引
+	public int queryNodeByName(String name) {
+		Node tmp=head;
+		//遍历节点
+		int index=0;
+		while (tmp!=null) {
+			index++;
+			if (tmp.name.equals(name)) {
+				return index;
+			}
+			tmp=tmp.next;			
+		}
+		return -1;
+	}
+	//修改
+	public void editNode(String name,int age) {
+		Node tmp=head;
+		while (tmp!=null) {
+			if (tmp.name.equals(name)) {
+				tmp.age=age;
+			}
+			tmp=tmp.next;
+		}
+	}
+	
+	//在指定位置添加node
+	public void insertNode(String name,Node node) {
+		if (head==null) {
+			//将头部指向node
+			head=node;
+			return;
+		}
+		Node tmp=head;
+		//遍历数组找到指定元素
+		while (tmp!=null) {			
+						
+			if (tmp.name.equals(name)) {		
+				node.next=tmp.next;
+				tmp.next=node;				
+			}
+			
+			tmp=tmp.next;
+		}
+	}
+	//查看所有节点
+	public void list() {
+		//遍历所有节点  从头开始
+		//把head赋值给一个临时变量
+		Node tmp=head;
+		//判断节点是否为空
+		while (tmp!=null) {
+			tmp.toString();//打印数据
+			tmp=tmp.next;
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		//初始化对象
+		LinklistDemo demo=new LinklistDemo();
+		//添加节点对象
+		demo.addNode(new Node("葬剑灬尊",21));
+		demo.addNode(new Node("浪子一秋",21));
+		demo.addNode(new Node("一叶知秋",21));
+		demo.addNode(new Node("剑侠情缘",21));
+		//打印节点
+		demo.list();
+		//删除节点
+		System.out.println("-----------删除-----------");
+		demo.deleteNode("葬剑灬尊");
+		demo.list();
+		//查询
+		System.out.println("--------查询----------");
+		int index=demo.queryNodeByName("剑侠情缘");
+		System.out.println(index);
+		//修改信息
+		System.out.println("--------修改------------");
+		demo.editNode("浪子一秋", 22);
+		demo.list();		
+		//添加
+		System.out.println("---------添加----------");
+		demo.insertNode("浪子一秋",new Node("吠舞罗",21));
+		demo.list();
+	}
+}
---
+++ src/day18/data1/DoubleLinklistDemo.java
@@ -0,0 +1,87 @@
+package day18.data1;
+
+
+//节点类
+class Node{
+	//节点
+	Node pre;//上个节点
+	Node next;//下个节点
+	//数据部分
+	String name;
+	int age;
+	public Node() {
+		// TODO 自动生成的构造函数存根
+	}
+	public  Node(String name,int age) {
+		this.name=name;
+		this.age=age;
+	}
+	@Override
+	public String toString() {
+		System.out.println("name:"+name+"\t\tage:"+age);
+		return super.toString();
+	}
+}
+public class DoubleLinklistDemo {
+	//操作双链表
+	//头部节点
+	Node head;
+	//尾部节点
+	Node foot;
+
+	//添加节点  尾部
+	public void addLast(Node node) {
+		//找尾部
+		if (foot==null) {
+			head=node;
+			foot=node;
+			return;
+		}
+		//赋值
+		foot.next=node;
+		node.pre=foot;
+		//foot指向最新的节点
+		foot=node;
+	}
+	//在头部添加
+	public void addFirst(Node node) {
+		//判断head是否为空
+		if (head==null) {
+			head=node;
+			foot=node;
+			return;
+		}
+		//赋值
+		node.next=head;
+		head.pre=node;
+		head=node;
+	}
+	//打印所有信息
+	public void list() {
+		//遍历
+		Node node=head;
+		while (node.next!=null) {
+			node.toString();
+			node=node.next;
+		}
+	}
+	public static void main(String[] args) {
+		// 初始化对象
+		DoubleLinklistDemo demo=new DoubleLinklistDemo();
+		//在头部添加节点对象
+		System.out.println("-----在头部添加------");
+		demo.addFirst(new Node("葬剑灬尊",21));
+		demo.addFirst(new Node("浪子一秋",21));
+		demo.addFirst(new Node("一叶知秋",21));
+		demo.addFirst(new Node("剑侠情缘",21));
+		//打印节点
+		demo.list();
+		//在尾部添加
+		System.out.println("--------在尾部添加----------");
+		demo.addLast(new Node("秦时明月",22));
+		demo.addLast(new Node("天行九歌",22));
+		demo.addLast(new Node("夜尽天明",22));
+		demo.list();
+	}
+
+}
